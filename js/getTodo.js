9a43225695b289a0e7d621ebfc37b32ff8cea10b
irmaodoguilherme <airesguilherme83@gmail.com>
2024-06-17 20:19:32 -0300

    testing responsiveness

--- js/getTodo.js
+++ js/getTodo.js
@@ -2,7 +2,7 @@ export const getTodo = inputValue => {
   const liTodo = document.createElement('li')
   liTodo.textContent = inputValue
   liTodo.setAttribute('data-todo', inputValue)
-  liTodo.classList.add('d-flex', 'justify-content-between', 'text-center')
+  liTodo.classList.add('d-flex', 'justify-content-between', 'text-break', 'gap-3')
 
   const trashIcon = document.createElement('i')
   trashIcon.classList.add('bi', 'bi-trash', 'd-flex', 'align-items-center')
