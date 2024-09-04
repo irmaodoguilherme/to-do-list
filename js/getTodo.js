da5a6ce3534b6d79901981a3db228d93c2232a96
irmaodoguilherme <airesguilherme83@gmail.com>
2024-09-04 12:20:28 -0300

    Acessibilidade implementada

--- js/getTodo.js
+++ js/getTodo.js
@@ -2,10 +2,10 @@ export const getTodo = inputValue => {
   const liTodo = document.createElement('li')
   liTodo.textContent = inputValue
   liTodo.setAttribute('data-todo', inputValue)
-  liTodo.classList.add('d-flex', 'justify-content-between', 'text-break', 'gap-3')
+  liTodo.setAttribute('class', 'd-flex justify-content-between text-break gap-3 px-3 py-1')
 
   const trashIcon = document.createElement('i')
-  trashIcon.classList.add('bi', 'bi-trash', 'd-flex', 'align-items-center')
+  trashIcon.setAttribute('class', 'bi bi-trash d-flex align-items-center')
   trashIcon.setAttribute('data-trash', inputValue)
 
   liTodo.append(trashIcon)
